961b2aef0a54b5809efaa22fcb4569aae4943562
Hemant mahor <hemantmahorwork@gmail.com>
2025-06-21 15:21:30 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,5 @@
 def call(String dockerHubUser, String projectName, String tag) {
+    echo "Building ${dockerHubUser}/${projectName}:${tage} image "
     def imageTag = "${dockerHubUser}/${projectName}:${tag}"
     sh "docker build -t ${imageTag} ."
 }
